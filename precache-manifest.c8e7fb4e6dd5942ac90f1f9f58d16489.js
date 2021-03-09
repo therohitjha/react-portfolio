bd3196444b642d1e4824ec2cc53e7b6f6f5f7d29
Rohit Jha <therohitjha@gmail.com>
2021-03-10 00:35:52 +0530

    Updates

---
+++ precache-manifest.c8e7fb4e6dd5942ac90f1f9f58d16489.js
@@ -0,0 +1,374 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ab3f4d96a82272e623d978351e8ab0",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "abab65b2d693318a76fa",
+    "url": "/react-portfolio/static/css/2.b71ebc9b.chunk.css"
+  },
+  {
+    "revision": "e90351c4a4a3c22d6071",
+    "url": "/react-portfolio/static/css/main.f8fd3397.chunk.css"
+  },
+  {
+    "revision": "abab65b2d693318a76fa",
+    "url": "/react-portfolio/static/js/2.13df6e8d.chunk.js"
+  },
+  {
+    "revision": "a8b7f9925879c67c458b78e4fa827b7f",
+    "url": "/react-portfolio/static/js/2.13df6e8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e90351c4a4a3c22d6071",
+    "url": "/react-portfolio/static/js/main.3e87b69d.chunk.js"
+  },
+  {
+    "revision": "c1e199bde4a1af72dc27",
+    "url": "/react-portfolio/static/js/runtime-main.2a391a4b.js"
+  },
+  {
+    "revision": "3722f7e79ce50cc4aac051312d4cb785",
+    "url": "/react-portfolio/static/media/AboutUsPage.3722f7e7.jpeg"
+  },
+  {
+    "revision": "df4fcdeb5b2c8cef1eef0da98e214e9f",
+    "url": "/react-portfolio/static/media/AgencyLandingPage.df4fcdeb.jpeg"
+  },
+  {
+    "revision": "7e8818a2ad17c1fbf0ed3bd196b01e5f",
+    "url": "/react-portfolio/static/media/BlogIndexPage.7e8818a2.jpeg"
+  },
+  {
+    "revision": "88a71e31c78ae889143e45546c5bef3b",
+    "url": "/react-portfolio/static/media/ContactUsPage.88a71e31.jpeg"
+  },
+  {
+    "revision": "7d7ae3a60315557b0293f2c27882ebe1",
+    "url": "/react-portfolio/static/media/EventLandingPage.7d7ae3a6.jpeg"
+  },
+  {
+    "revision": "29cc9383e3a0f2fc369ad9a5ad6e0db4",
+    "url": "/react-portfolio/static/media/HostingCloudLandingPage.29cc9383.jpeg"
+  },
+  {
+    "revision": "4fde2dfbce6e91ebf4588557d6f77a4c",
+    "url": "/react-portfolio/static/media/HotelTravelLandingPage.4fde2dfb.jpeg"
+  },
+  {
+    "revision": "d5a48afbe6bab4933939eff7653758fb",
+    "url": "/react-portfolio/static/media/LoginPage.d5a48afb.jpeg"
+  },
+  {
+    "revision": "f3822e94600621912441e404b6e9c98f",
+    "url": "/react-portfolio/static/media/PricingPage.f3822e94.jpeg"
+  },
+  {
+    "revision": "60bc4cc11478f8bb00e285f976a88cf8",
+    "url": "/react-portfolio/static/media/PrivacyPolicyPage.60bc4cc1.jpeg"
+  },
+  {
+    "revision": "b747f875e940514cbef7962de9f8e9b2",
+    "url": "/react-portfolio/static/media/RestaurantLandingPage.b747f875.jpeg"
+  },
+  {
+    "revision": "bea0b53ccea78a60bf883aa536b4f09c",
+    "url": "/react-portfolio/static/media/SaaSProductLandingPage.bea0b53c.jpeg"
+  },
+  {
+    "revision": "41a344b7ce90bf5410e7bd4099dd6506",
+    "url": "/react-portfolio/static/media/ServiceLandingPage.41a344b7.jpeg"
+  },
+  {
+    "revision": "1cecad526a9a5088f70103ff8c43e033",
+    "url": "/react-portfolio/static/media/SignupPage.1cecad52.jpeg"
+  },
+  {
+    "revision": "15d07b59f932f01bc25e4bcaa871bb1a",
+    "url": "/react-portfolio/static/media/TermsOfServicePage.15d07b59.jpeg"
+  },
+  {
+    "revision": "92f0fc5998d20d7219db2625993aa34f",
+    "url": "/react-portfolio/static/media/app-mockup.92f0fc59.png"
+  },
+  {
+    "revision": "319a321f4d259ea75f3a12ca091692c0",
+    "url": "/react-portfolio/static/media/arrow-left-2-icon.319a321f.svg"
+  },
+  {
+    "revision": "41406a779fc77849c5e239173f4c4724",
+    "url": "/react-portfolio/static/media/arrow-left-3-icon.41406a77.svg"
+  },
+  {
+    "revision": "42c238c46338189c711d0b6223cd220b",
+    "url": "/react-portfolio/static/media/arrow-right-2-icon.42c238c4.svg"
+  },
+  {
+    "revision": "e5cd3c02a99df7a0698490d716530206",
+    "url": "/react-portfolio/static/media/arrow-right-3-icon.e5cd3c02.svg"
+  },
+  {
+    "revision": "348807c7ecb7270d15fccff16ffe3fe4",
+    "url": "/react-portfolio/static/media/arrow-right-icon.348807c7.svg"
+  },
+  {
+    "revision": "8aa459ae951030ec44cb20c20de623c0",
+    "url": "/react-portfolio/static/media/briefcase.8aa459ae.svg"
+  },
+  {
+    "revision": "daa6dd44d99ff512fa3fdfd824cf442d",
+    "url": "/react-portfolio/static/media/celebration-icon.daa6dd44.svg"
+  },
+  {
+    "revision": "ce63c0459a272d8301b186ec73db37e7",
+    "url": "/react-portfolio/static/media/check-circle.ce63c045.svg"
+  },
+  {
+    "revision": "ce57d5db865a8c5370d0de567516bb3b",
+    "url": "/react-portfolio/static/media/checkbox-circle.ce57d5db.svg"
+  },
+  {
+    "revision": "09cc57a3737648e3052af81e0c83e65c",
+    "url": "/react-portfolio/static/media/chef-icon.09cc57a3.svg"
+  },
+  {
+    "revision": "c66b213d1dfff391c61146727032f068",
+    "url": "/react-portfolio/static/media/chevron-down.c66b213d.svg"
+  },
+  {
+    "revision": "5727f869509c87f4d25c2225f0968573",
+    "url": "/react-portfolio/static/media/chevron-left.5727f869.svg"
+  },
+  {
+    "revision": "9c59704b1de34c9fbb54c100b907ac85",
+    "url": "/react-portfolio/static/media/chevron-right.9c59704b.svg"
+  },
+  {
+    "revision": "ea3f87d138d7aecf39f43afa7707da47",
+    "url": "/react-portfolio/static/media/clock.ea3f87d1.svg"
+  },
+  {
+    "revision": "6e80729ab3496e4871f5f183b0b4c9c7",
+    "url": "/react-portfolio/static/media/customer-support-illustration.6e80729a.svg"
+  },
+  {
+    "revision": "5f12b0dc993b9ea0bb9c928d98f89c07",
+    "url": "/react-portfolio/static/media/customers-logo-strip.5f12b0dc.png"
+  },
+  {
+    "revision": "7c1a6f97bf87de69a244342e6c17389c",
+    "url": "/react-portfolio/static/media/customize-icon.7c1a6f97.svg"
+  },
+  {
+    "revision": "ee1c0c55aa99cd9524551ef19b86aad3",
+    "url": "/react-portfolio/static/media/design-illustration-2.ee1c0c55.svg"
+  },
+  {
+    "revision": "3ac89e2d233af035e25ca8334d179934",
+    "url": "/react-portfolio/static/media/design-illustration.3ac89e2d.svg"
+  },
+  {
+    "revision": "cbeab2705cd1a8c5d774eac8ed9c9619",
+    "url": "/react-portfolio/static/media/dollar-sign.cbeab270.svg"
+  },
+  {
+    "revision": "1c251c8ff2222207650b4f24a2159cbb",
+    "url": "/react-portfolio/static/media/dot-pattern.1c251c8f.svg"
+  },
+  {
+    "revision": "a1ad24916d979e088c5e8c80b073f92b",
+    "url": "/react-portfolio/static/media/email-illustration.a1ad2491.svg"
+  },
+  {
+    "revision": "7064f155dc1611550f5ade904f1964e3",
+    "url": "/react-portfolio/static/media/email-newsletter-icon.7064f155.svg"
+  },
+  {
+    "revision": "5e030c93ee64514b9fcdc887289ce8a9",
+    "url": "/react-portfolio/static/media/facebook-icon.5e030c93.svg"
+  },
+  {
+    "revision": "6c9e80eec6977f6497b0e55f0fce374a",
+    "url": "/react-portfolio/static/media/fast-icon.6c9e80ee.svg"
+  },
+  {
+    "revision": "61eae8b2845b7d35c3d58786733ea3d2",
+    "url": "/react-portfolio/static/media/github-icon.61eae8b2.svg"
+  },
+  {
+    "revision": "1669eb96c162ff86e8318d02b7f89f24",
+    "url": "/react-portfolio/static/media/hero-screenshot-1.1669eb96.png"
+  },
+  {
+    "revision": "31c082beabc66a4c2bc8da2da668ef08",
+    "url": "/react-portfolio/static/media/hero-screenshot-2.31c082be.png"
+  },
+  {
+    "revision": "fa5666fa9730a9f927a76edfa9b53e2b",
+    "url": "/react-portfolio/static/media/linkedin-icon.fa5666fa.svg"
+  },
+  {
+    "revision": "6c3aec23252e8674def010db7d1c3b78",
+    "url": "/react-portfolio/static/media/log-in.6c3aec23.svg"
+  },
+  {
+    "revision": "60e77202beb2649ae04467f515de60ad",
+    "url": "/react-portfolio/static/media/login-illustration.60e77202.svg"
+  },
+  {
+    "revision": "0a4fa34ed3a8384ba623ca8939f8b679",
+    "url": "/react-portfolio/static/media/logo-light.0a4fa34e.svg"
+  },
+  {
+    "revision": "f9776f55173cd40b2b3f3fed1c7443c4",
+    "url": "/react-portfolio/static/media/logo.f9776f55.svg"
+  },
+  {
+    "revision": "58383984fcf7cd1ac831eb8d9b9a86ca",
+    "url": "/react-portfolio/static/media/love-illustration.58383984.svg"
+  },
+  {
+    "revision": "b2f804bda31dee415fc2b1ff7ba9315f",
+    "url": "/react-portfolio/static/media/map-pin.b2f804bd.svg"
+  },
+  {
+    "revision": "ca394ec8a4754ab9db293dcac626bdcb",
+    "url": "/react-portfolio/static/media/menu.ca394ec8.svg"
+  },
+  {
+    "revision": "9661ba0b1e7abb32b6b4b956d5d14899",
+    "url": "/react-portfolio/static/media/minus.9661ba0b.svg"
+  },
+  {
+    "revision": "36f0e6ee645c508fdc115426b87baba6",
+    "url": "/react-portfolio/static/media/play-circle.36f0e6ee.svg"
+  },
+  {
+    "revision": "7c6c8212afc5044e4b60f59552e57e69",
+    "url": "/react-portfolio/static/media/plus.7c6c8212.svg"
+  },
+  {
+    "revision": "fe9ecccb574586db8a2ffe7d22b86a98",
+    "url": "/react-portfolio/static/media/prototype-illustration.fe9ecccb.svg"
+  },
+  {
+    "revision": "1de1406c7d731eac5d1fe0c638eab549",
+    "url": "/react-portfolio/static/media/quotes-l.1de1406c.svg"
+  },
+  {
+    "revision": "1b5068d7d77725183060ce7dd9d6603c",
+    "url": "/react-portfolio/static/media/quotes-r.1b5068d7.svg"
+  },
+  {
+    "revision": "a12572bdffec3f4a0b1df5e8a332a139",
+    "url": "/react-portfolio/static/media/reliable-icon.a12572bd.svg"
+  },
+  {
+    "revision": "973a848d54985d018e528578a5c3bc37",
+    "url": "/react-portfolio/static/media/server-illustration-2.973a848d.svg"
+  },
+  {
+    "revision": "657d5c583a8d1d6dd7188cf8344de6ed",
+    "url": "/react-portfolio/static/media/server-redundancy-illustration.657d5c58.svg"
+  },
+  {
+    "revision": "0af0a49c9cbe4c46ba2e84f1748865a1",
+    "url": "/react-portfolio/static/media/server-secure-illustration.0af0a49c.svg"
+  },
+  {
+    "revision": "297d50a2cd8b864f0c1f357e5108d87a",
+    "url": "/react-portfolio/static/media/shield-icon.297d50a2.svg"
+  },
+  {
+    "revision": "6c8f89cc4172b2cb49caeeef863d9692",
+    "url": "/react-portfolio/static/media/shop-icon.6c8f89cc.svg"
+  },
+  {
+    "revision": "527babdb98bf8a457080cb1de19ebfa1",
+    "url": "/react-portfolio/static/media/signup-illustration.527babdb.svg"
+  },
+  {
+    "revision": "345871b443e4f5f841bedccf11a61859",
+    "url": "/react-portfolio/static/media/simple-icon.345871b4.svg"
+  },
+  {
+    "revision": "5d0ff9bc5818d6498df9d90437127f90",
+    "url": "/react-portfolio/static/media/star-icon.5d0ff9bc.svg"
+  },
+  {
+    "revision": "ac8ea2135e1379656f02ca2eea26cc87",
+    "url": "/react-portfolio/static/media/star.ac8ea213.svg"
+  },
+  {
+    "revision": "a200bad45b923b0b206917b16755f83f",
+    "url": "/react-portfolio/static/media/stats-illustration.a200bad4.svg"
+  },
+  {
+    "revision": "135d403ccd52c50bf2e773a8a68cea57",
+    "url": "/react-portfolio/static/media/support-icon.135d403c.svg"
+  },
+  {
+    "revision": "ec52da39f670db5b4b3c32f54914a919",
+    "url": "/react-portfolio/static/media/svg-decorator-blob-1.ec52da39.svg"
+  },
+  {
+    "revision": "159be2476eedd8b63bb0c7e0a58d0e47",
+    "url": "/react-portfolio/static/media/svg-decorator-blob-3.159be247.svg"
+  },
+  {
+    "revision": "06b7e9297b931c8f3c729d9666faf31f",
+    "url": "/react-portfolio/static/media/svg-decorator-blob-4.06b7e929.svg"
+  },
+  {
+    "revision": "50a3868db49539d53742411308690eba",
+    "url": "/react-portfolio/static/media/svg-decorator-blob-5.50a3868d.svg"
+  },
+  {
+    "revision": "5b0723ee967e9551b9e35d6968869099",
+    "url": "/react-portfolio/static/media/svg-decorator-blob-6.5b0723ee.svg"
+  },
+  {
+    "revision": "c86fb21a2396dc9a8149429f1cd451cf",
+    "url": "/react-portfolio/static/media/svg-decorator-blob-7.c86fb21a.svg"
+  },
+  {
+    "revision": "19df9c1d30e1ee23905d4034a5eb5da3",
+    "url": "/react-portfolio/static/media/svg-decorator-blob-8.19df9c1d.svg"
+  },
+  {
+    "revision": "06b7e9297b931c8f3c729d9666faf31f",
+    "url": "/react-portfolio/static/media/svg-decorator-blob-9.06b7e929.svg"
+  },
+  {
+    "revision": "9317bc9dac095723b9f4afd2abaf7b15",
+    "url": "/react-portfolio/static/media/tag.9317bc9d.svg"
+  },
+  {
+    "revision": "5ae3fe7a0d10e5c14ec0575db276dcda",
+    "url": "/react-portfolio/static/media/team-illustration-2.5ae3fe7a.svg"
+  },
+  {
+    "revision": "4e2c0a1d4a1adb1a99a69310e06fbbc7",
+    "url": "/react-portfolio/static/media/trending-up.4e2c0a1d.svg"
+  },
+  {
+    "revision": "7189052ca672d113f057d9a4f3cb5efe",
+    "url": "/react-portfolio/static/media/twitter-icon.7189052c.svg"
+  },
+  {
+    "revision": "5047d00469e004c51f6b14e245c66d6f",
+    "url": "/react-portfolio/static/media/user-plus.5047d004.svg"
+  },
+  {
+    "revision": "ed52f187b2c4aad7fd02765c9cdc2f92",
+    "url": "/react-portfolio/static/media/user.ed52f187.svg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/react-portfolio/static/media/x.b0200bf7.svg"
+  },
+  {
+    "revision": "45e500e6c6c6577b063b28adc39fb422",
+    "url": "/react-portfolio/static/media/youtube-icon.45e500e6.svg"
+  }
+]);
